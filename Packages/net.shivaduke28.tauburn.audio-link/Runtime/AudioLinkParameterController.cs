343e09a2e30b0d6077aaa2df2b596f655078c8fb
shivaduke28 <shivaduke28@gmail.com>
2022-11-19 06:40:23 +0900

    implement AudioLink bridge classes

---
+++ Packages/net.shivaduke28.tauburn.audio-link/Runtime/AudioLinkParameterController.cs
@@ -0,0 +1,33 @@
+﻿using Tauburn.Core;
+using UnityEngine;
+using VRCAudioLink;
+
+namespace Tauburn.AudioLink
+{
+    public class AudioLinkParameterController : IntParameterHandler
+    {
+        [SerializeField] AudioLinkPreset[] audioLinkPresets;
+        [SerializeField] AudioLinkFloatParameter[] audioLinkFloatParameters;
+        [SerializeField] AudioLinkController audioLinkController;
+
+        void Start()
+        {
+            foreach (var audioLinkPreset in audioLinkPresets)
+            {
+                audioLinkPreset.Initialize(audioLinkController);
+            }
+            foreach (var audioLinkFloatParameter in audioLinkFloatParameters)
+            {
+                audioLinkFloatParameter.Initialize(audioLinkController);
+            }
+        }
+
+        public override void Set(int value)
+        {
+            if (0 <= value && value < audioLinkPresets.Length)
+            {
+                audioLinkPresets[value].Load();
+            }
+        }
+    }
+}
