8061c77ef52766346ce353b75d19ec2a6cceb11f
shivaduke28 <shivaduke28@gmail.com>
2022-12-11 03:56:41 +0900

    MidiAssignControllerEditor

---
+++ Packages/net.shivaduke28.tauburn/Editor/Midi/MidiAssignControllerEditor.cs
@@ -0,0 +1,48 @@
+﻿using Tauburn.Midi;
+using UdonSharpEditor;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor.Midi
+{
+    [CustomEditor(typeof(MidiAssignController))]
+    public sealed class MidiAssignControllerEditor : UnityEditor.Editor
+    {
+        MidiAssignController midiAssignController;
+        SerializedProperty midiFloatInputs;
+        SerializedProperty midiIntInputs;
+
+        void OnEnable()
+        {
+            midiAssignController = (MidiAssignController) target;
+            midiFloatInputs = serializedObject.FindProperty(nameof(midiFloatInputs));
+            midiIntInputs = serializedObject.FindProperty(nameof(midiIntInputs));
+        }
+
+        public override void OnInspectorGUI()
+        {
+            if (UdonSharpGUI.DrawDefaultUdonSharpBehaviourHeader(target, false, false)) return;
+            base.OnInspectorGUI();
+
+            GUILayout.Space(10f);
+            if (GUILayout.Button("Collect all MIDI Inputs in Children."))
+            {
+                var floatInputs = midiAssignController.GetComponentsInChildren<MidiFloatInput>(true);
+                var intInputs = midiAssignController.GetComponentsInChildren<MidiIntInput>(true);
+                midiFloatInputs.arraySize = floatInputs.Length;
+                midiIntInputs.arraySize = intInputs.Length;
+
+                for (var i = 0; i < floatInputs.Length; i++)
+                {
+                    midiFloatInputs.GetArrayElementAtIndex(i).objectReferenceValue = floatInputs[i];
+                }
+
+                for (var i = 0; i < intInputs.Length; i++)
+                {
+                    midiIntInputs.GetArrayElementAtIndex(i).objectReferenceValue = intInputs[i];
+                }
+                serializedObject.ApplyModifiedProperties();
+            }
+        }
+    }
+}
